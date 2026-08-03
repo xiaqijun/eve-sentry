998eedb5f9e27e0f2f51d90bcd12db91fc5af817
EVE Sentry Dev <dev@eve-sentry.local>
2026-08-04 03:29:19 +0800

    chore: release client 1.0.16

--- app/version.py
+++ app/version.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import os
 
 
-APP_VERSION = "1.0.15"
+APP_VERSION = "1.0.16"
 DEFAULT_UPDATE_MANIFEST_URL = (
     "https://evesentrydownload.kisectool.com/latest.json"
 )
