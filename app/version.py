7f6c784dc90f5dd92575e3a02e161321a40ad82c
xiaqiqi <xiaqiqi@outlook.com>
2026-09-01 20:21:29 +0800

    chore: bump client version to 1.0.54

--- app/version.py
+++ app/version.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import os
 
 
-APP_VERSION = "1.0.53"
+APP_VERSION = "1.0.54"
 DEFAULT_UPDATE_MANIFEST_URL = (
     "https://evesentrydownload.kisectool.com/latest.json"
 )
