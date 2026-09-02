c7c66fe0179d7f209bdfdc699906ea9a7a3da79a
xiaqiqi <xiaqiqi@outlook.com>
2026-09-02 18:51:50 +0800

    chore: release client 1.0.56

--- app/version.py
+++ app/version.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import os
 
 
-APP_VERSION = "1.0.55"
+APP_VERSION = "1.0.56"
 DEFAULT_UPDATE_MANIFEST_URL = (
     "https://evesentrydownload.kisectool.com/latest.json"
 )
